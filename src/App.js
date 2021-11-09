1fdc7c45f35e0892394d00d488c38a4e6d9a69bb
ilijapav <ilija.pavlovic@proudsourceit.rs>
2021-11-09 21:41:59 +0100

    Adds question.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const startDate = new Date("2021-11-19T16:00Z");
+  const miliseconds = Math.abs(startDate - new Date());
+  const seconds = miliseconds / 1000;
+  const minutes = miliseconds / 1000 / 60;
+  const hours = miliseconds / 1000 / 60 / 60;
+  const days = miliseconds / 1000 / 60 / 60 / 24;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="heading">
+        <h1>When the Wheel of Time starts ?</h1>
+        <h2>Choose someone to give you AN answer :)</h2>
+        {/* <h3>{startDate.toLocaleString()}</h3>
+        <h3>Miliseconds: {miliseconds}</h3>
+        <h3>Seconds: {seconds.toFixed(2)}</h3>
+        <h3>Minutes: {minutes.toFixed(2)}</h3>
+        <h3>Hours: {hours.toFixed(2)}</h3>
+        <h3>Days: {days.toFixed(2)}</h3> */}
+      </div>
+      <div className="team-wrapper"></div>
     </div>
   );
 }
