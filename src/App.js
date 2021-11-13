f5957851bf2f771851775bdbf22f609c73026c07
ilijapavlovic95 <ilija95pav@gmail.com>
2021-11-13 01:22:07 +0100

    Redesign.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,61 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
-import Character from './components/Character';
-import { ANSWERS, CHARACTERS } from './data';
+import { CHARACTERS } from './data';
 
 
 function App() {
   const startDate = new Date('2021-11-19T01:00Z');
+  const title = 'The Wheel Of Timer';
 
-  const [characters, setCharacters] = useState([]);
-  const [answers, setAnswers] = useState([]);
+  const [themes, setThemes] = useState(null);
+  const [timer, setTimer] = useState(null);
+  const [theme, setTheme] = useState(null);
 
   useEffect(() => {
-    setCharacters(shuffleArray(CHARACTERS));
-    setAnswers(ANSWERS);
+    setThemes(shuffleArray(CHARACTERS));
+    calculateTimer();
+    setInterval(() => {
+      calculateTimer();
+    }, 1000);
   }, []);
+
+  useEffect(() => {
+    console.log(themes);
+    if (themes && !theme) {
+      setTheme(themes.find(el => el.id === 'md'));
+    }
+  }, [themes]);
+
+  const calculateTimer = () => {
+    const miliseconds = Math.abs(startDate - new Date());
+    const days = Math.floor(miliseconds / 1000 / 60 / 60 / 24);
+    const hours = Math.floor(miliseconds / 1000 / 60 / 60 - days * 24);
+    const minutes = Math.floor(miliseconds / 1000 / 60 - days * 24 * 60 - hours * 60);
+    const seconds = Math.floor(miliseconds / 1000 - days * 24 * 60 * 60 - hours * 60 * 60 - minutes * 60);
+    const daysStr = days < 10 ? '0' + days : days.toString();
+    const hoursStr = hours < 10 ? '0' + hours : hours.toString();
+    const minutesStr = minutes < 10 ? '0' + minutes : minutes.toString();
+    const secondsStr = seconds < 10 ? '0' + seconds : seconds.toString();
+    setTimer([{ label: 'days', value: daysStr }, { label: 'hours', value: hoursStr }, { label: 'minutes', value: minutesStr }, { label: 'seconds', value: secondsStr }]);
+  };
+
+  const switchToPrevTheme = () => {
+    const index = themes.findIndex(el => el.id === theme.id);
+    if (index === 0) {
+      setTheme(themes[themes.length - 1]);
+    } else {
+      setTheme(themes[index - 1]);
+    }
+  };
+
+  const switchToNextTheme = () => {
+    const index = themes.findIndex(el => el.id === theme.id);
+    if (index === themes.length - 1) {
+      setTheme(themes[0]);
+    } else {
+      setTheme(themes[index + 1]);
+    }
+  };
 
   const shuffleArray = (unshuffledArray) => {
     return unshuffledArray
@@ -22,13 +64,68 @@ function App() {
       .map(({ value }) => value);
   };
 
-  return (
-    <div className="App">
-      <div className="heading">
-        <h1>The Wheel of Timer</h1>
-        <h2>Click on someone to give you AN answer how many days are left :)</h2>
+  return theme && (
+    <div className="App" style={{ '--timer-background': theme.color, '--timer-color': 'white' }}>
+      {/* <div className="heading">
+        <h1>
+          {title.split(' ').map(word => (
+            <span key={word}>
+              {[...word].map(char => (
+                <span key={char}>{char}</span>
+              ))}
+            </span>
+            
+          ))}
+        </h1>
+      </div> */}
+      <div className="theme">
+        <div className="chooser-label">CHOOSE THEME</div>
+        <div className="theme-chooser">
+          <div className="prev-theme arrow" onClick={switchToPrevTheme}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z" /></svg>
+          </div>
+          <div className="current-theme">{theme.theme}</div>
+          <div className="next-theme arrow" onClick={switchToNextTheme}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M7.33 24l-2.83-2.829 9.339-9.175-9.339-9.167 2.83-2.829 12.17 11.996z" /></svg>
+          </div>
+        </div>
       </div>
-      <div className="team-wrapper">
+
+      <div className="wot-timer">
+
+        <div>
+          <div className="timer-title">
+            WHEEL OF TIMER
+          </div>
+          <div className="values">
+            {
+              timer && timer.map(col =>
+                <div key={col.label} className="timer-col value">
+                  {[...col.value].map((char, charIndex) => (<span key={charIndex}>{char}</span>))}
+                </div>
+              )
+            }
+          </div>
+          <div className="labels">
+            <div className="timer-col label">
+              <div>days</div>
+            </div>
+            <div className="timer-col label">
+              <div>hours</div>
+            </div>
+            <div className="timer-col label">
+              <div>minutes</div>
+            </div>
+            <div className="timer-col label">
+              <div>seconds</div>
+            </div>
+          </div>
+        </div>
+
+
+      </div>
+
+      {/* <div className="team-wrapper">
         {characters.map((el) => {
           return (
             <div className="char-wrapper" key={el.id}>
@@ -42,7 +139,7 @@ function App() {
             </div>
           );
         })}
-      </div>
+      </div> */}
     </div>
   );
 }
--- src/data.js
+++ src/data.js
@@ -1,131 +1,138 @@
 const CHARACTERS = [
   {
-    id: "r",
-    code: "rand",
-    name: "Rand",
-    color: "#d63031",
+    id: 'r',
+    code: 'rand',
+    name: 'Rand',
+    color: '#d63031',
+    theme: 'Sheepherder'
   },
   {
-    id: "e",
-    code: "egwene",
-    name: "Egwene",
-    color: "#00b894",
+    id: 'e',
+    code: 'egwene',
+    name: 'Egwene',
+    color: '#00b894',
+    theme: 'Dreamer'
   },
   {
-    id: "n",
-    code: "nynaeve",
-    name: "Nynaeve",
-    color: "#fdcb6e",
+    id: 'n',
+    code: 'nynaeve',
+    name: 'Nynaeve',
+    color: '#fed330',
+    theme: 'Healer'
   },
   {
-    id: "mc",
-    code: "mat",
-    name: "Mat",
-    color: "#636e72",
+    id: 'mc',
+    code: 'mat',
+    name: 'Mat',
+    color: '#636e72',
+    theme: 'Gambler'
   },
   {
-    id: "md",
-    code: "moiraine",
-    name: "Moiraine",
-    color: "#0984e3",
+    id: 'md',
+    code: 'moiraine',
+    name: 'Moiraine',
+    color: '#0984e3',
+    theme: 'Aes Sedai'
   },
   {
-    id: "l",
-    code: "lan",
-    name: "Lan",
-    color: "#b2bec3",
+    id: 'l',
+    code: 'lan',
+    name: 'Lan',
+    color: '#b2bec3',
+    theme: 'Dai Shan'
   },
   {
-    id: "p",
-    code: "perrin",
-    name: "Perrin",
-    color: "#ffeaa7",
+    id: 'p',
+    code: 'perrin',
+    name: 'Perrin',
+    color: '#ffa502',
+    theme: 'Young Bull'
   },
 ];
 
-const ANSWERS = [
-  // mat
-  {
-    id: "mc1",
-    character: "mc",
-    answer: "Blood and bloody ashes, I don't know!",
-  },
-  {
-    id: "mc2",
-    character: "mc",
-    answer: "Ask Perrin or Rand. They'll know.",
-  },
-  // moiraine
-  {
-    id: "md1",
-    character: "md",
-    answer: "There are neither beginnings nor endings to the Wheel of Time.",
-  },
-  {
-    id: "md2",
-    character: "md",
-    answer: "I'll tell you when the time comes.",
-  },
-  // lan
-  {
-    id: "l1",
-    character: "l",
-    answer: "...",
-  },
-  {
-    id: "l2",
-    character: "l",
-    answer: ":days: days.",
-  },
-  // nynaeve
-  {
-    id: "n1",
-    character: "n",
-    answer: "Name yourself, stranger.",
-  },
-  {
-    id: "n2",
-    character: "n",
-    answer: "It's Women's Circle business.",
-  },
-  // perrin
-  {
-    id: "p1",
-    character: "p",
-    answer: "Ask Mat or Rand. They'll know.",
-  },
-  {
-    id: "p2",
-    character: "p",
-    answer: "It starts in :days: days. But give me a minute, just to check once more.",
-  },
-  // rand
-  {
-    id: "r1",
-    character: "r",
-    answer: "Who are you? Did you see my sheep?",
-  },
-  {
-    id: "r2",
-    character: "r",
-    answer: "Ask Perrin or Mat. They'll know.",
-  },
-  // egwene
-  {
-    id: "e1",
-    character: "e",
-    answer: ":days: days and :hours: hours. Do you need minutes?",
-  },
-  {
-    id: "e2",
-    character: "e",
-    answer: ":days: days, :hours: hours and :minutes: minutes. You don't want seconds? Why not?",
-  },
-  {
-    id: "e3",
-    character: "e",
-    answer: ":days: days, :hours: hours, :minutes: minutes and :seconds: seconds",
-  },
-];
+// const ANSWERS = [
+//   // mat
+//   {
+//     id: 'mc1',
+//     character: 'mc',
+//     answer: 'Blood and bloody ashes, I don]'t know!',
+//   },
+//   {
+//     id: 'mc2',
+//     character: 'mc',
+//     answer: 'Ask Perrin or Rand. They\'ll know.',
+//   },
+//   // moiraine
+//   {
+//     id: 'md1',
+//     character: 'md',
+//     answer: 'There are neither beginnings nor endings to the Wheel of Time.',
+//   },
+//   {
+//     id: 'md2',
+//     character: 'md',
+//     answer: 'I'll tell you when the time comes.',
+//   },
+//   // lan
+//   {
+//     id: 'l1',
+//     character: 'l',
+//     answer: '...',
+//   },
+//   {
+//     id: 'l2',
+//     character: 'l',
+//     answer: ':days: days.',
+//   },
+//   // nynaeve
+//   {
+//     id: 'n1',
+//     character: 'n',
+//     answer: 'Name yourself, stranger.',
+//   },
+//   {
+//     id: 'n2',
+//     character: 'n',
+//     answer: 'It's Women's Circle business.',
+//   },
+//   // perrin
+//   {
+//     id: 'p1',
+//     character: 'p',
+//     answer: 'Ask Mat or Rand. They'll know.',
+//   },
+//   {
+//     id: 'p2',
+//     character: 'p',
+//     answer: 'It starts in :days: days. But give me a minute, just to check once more.',
+//   },
+//   // rand
+//   {
+//     id: 'r1',
+//     character: 'r',
+//     answer: 'Who are you? Did you see my sheep?',
+//   },
+//   {
+//     id: 'r2',
+//     character: 'r',
+//     answer: 'Ask Perrin or Mat. They'll know.',
+//   },
+//   // egwene
+//   {
+//     id: 'e1',
+//     character: 'e',
+//     answer: ':days: days and :hours: hours. Do you need minutes?',
+//   },
+//   {
+//     id: 'e2',
+//     character: 'e',
+//     answer: ':days: days, :hours: hours and :minutes: minutes. You don't want seconds? Why not?',
+//   },
+//   {
+//     id: 'e3',
+//     character: 'e',
+//     answer: ':days: days, :hours: hours, :minutes: minutes and :seconds: seconds',
+//   },
+// ];
 
-export { CHARACTERS, ANSWERS };
+export { CHARACTERS };
